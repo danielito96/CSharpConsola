face321380092a29173df658021ddba46317ff37
danielito96 <daniel_m9603@hotmail.com>
2018-07-04 11:49:20 -0500

    respositorio nuevo

---
+++ CSharpConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
